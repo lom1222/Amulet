a247ec625a497208df6649784217a9126987f084
Nikita <imasmurf1222@gmail.com>
2026-02-17 23:05:35 -0500

    work on log

--- scripts/attached/log.gd
+++ scripts/attached/log.gd
@@ -5,6 +5,10 @@ func _ready() -> void:
 	text = ""
 	LogManager.message_logged.connect(_on_message_logged)
 	
+	LogManager.add_log("You awake.")
+	LogManager.add_log("The first thing you see are your hands, and in them, a shinning amulet.")
+	LogManager.add_log("You put it on, and tear your focus away. You are hungry. You dont know where you are.")
+	
 func _on_message_logged(_message_text: String):
 	clear()
 	for entry in LogManager.message_history:
